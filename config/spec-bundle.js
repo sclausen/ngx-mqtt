5ab74ed1c798b0b31b527d78f6b9d3d382c93d1d
Christofer Steingrefer <csteingrefer@gmail.com>
2018-05-07 10:25:06 +0200

    Remove rxjs compat

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -4,13 +4,7 @@ require('core-js/es6');
 require('core-js/es7/reflect');
 require('ts-helpers');
 require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy');
-require('zone.js/dist/jasmine-patch');
-require('rxjs/Rx');
+require('zone.js/dist/zone-testing');
 
 var testing = require('@angular/core/testing');
 var browser = require('@angular/platform-browser-dynamic/testing');
