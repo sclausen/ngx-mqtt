3644e9de856485ca69b7ffe33e86b9575dbd0c2b
Sebastian Clausen <github@sebastian-clausen.de>
2020-11-23 20:18:28 +0100

    fix: fixed installing with angular 11

--- projects/ngx-mqtt/postinstall.js
+++ projects/ngx-mqtt/postinstall.js
@@ -4,7 +4,7 @@ if (process.env['MQTT_DISABLE_HOOK']) {
 }
 
 const fs = require('fs');
-const f = '../../node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const f = '../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
 
 fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
