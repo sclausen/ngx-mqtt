271120af23e9c7989a5a39a199d90427037b2ab2
安泊尔 <shuangyan_m@hotmail.com>
2020-12-24 15:51:07 +0800

    Update postinstall.js
    根据ng版本修改路径

--- projects/ngx-mqtt/postinstall.js
+++ projects/ngx-mqtt/postinstall.js
@@ -1,10 +1,26 @@
+const path = require("path");
+const fs = require("fs");
+const semver_1 = require("semver");
 if (process.env['MQTT_DISABLE_HOOK']) {
   console.info('NGX-MQTT: postinstall hook disabled, MQTT_DISABLE_HOOK')
   return 0;
 }
 
-const fs = require('fs');
-const f = '../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
+let f ="../../node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
+const cwd = process.cwd();
+const packageJsonPath = path.join(cwd, "node_modules/@angular/core/package.json");
+if (fs.existsSync(packageJsonPath)) {
+  const content = fs.readFileSync(packageJsonPath, "utf-8");
+  if (content) {
+    const { version } = JSON.parse(content);
+    if (version) {
+      let semv = new semver_1.SemVer(version);
+      if(semv.major>=11){
+        f = '../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
+      }
+    }
+  }
+}
 
 fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
