50b6b55a62c32ea9efc1ec3dd57ea7811ef06f83
Sebastian Clausen <github@sebastian-clausen.de>
2021-01-08 11:20:31 +0100

    fixed issue when installing with different angular versions

--- projects/ngx-mqtt/postinstall.js
+++ projects/ngx-mqtt/postinstall.js
@@ -1,28 +1,33 @@
 const path = require("path");
 const fs = require("fs");
 const semver_1 = require("semver");
-if (process.env['MQTT_DISABLE_HOOK']) {
-  console.info('NGX-MQTT: postinstall hook disabled, MQTT_DISABLE_HOOK')
+if (process.env["MQTT_DISABLE_HOOK"]) {
+  console.info("NGX-MQTT: postinstall hook disabled, MQTT_DISABLE_HOOK");
   return 0;
 }
 
-let f ="../../node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
+let f =
+  "../../node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
 const cwd = process.cwd();
-const packageJsonPath = path.join(cwd, "node_modules/@angular/core/package.json");
+const packageJsonPath = path.join(
+  cwd,
+  "node_modules/@angular/core/package.json"
+);
 if (fs.existsSync(packageJsonPath)) {
   const content = fs.readFileSync(packageJsonPath, "utf-8");
   if (content) {
     const { version } = JSON.parse(content);
     if (version) {
       let semv = new semver_1.SemVer(version);
-      if(semv.major>=11){
-        f = '../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
+      if (semv.major >= 11) {
+        f =
+          "../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js";
       }
     }
   }
 }
 
-fs.readFile(f, 'utf8', function (err,data) {
+fs.readFile(f, "utf8", function (err, data) {
   if (err) {
     console.error(err);
     throw err;
@@ -32,8 +37,8 @@ fs.readFile(f, 'utf8', function (err,data) {
   // node: false
   // to:
   // node: {global: true}
-  const result = (data.replace(/node: false/g, "node: {global: true}"));
-  fs.writeFile(f, result, 'utf8', function (err) {
+  const result = data.replace(/node: false/g, "node: {global: true}");
+  fs.writeFile(f, result, "utf8", function (err) {
     if (err) {
       console.error(err);
       throw err;
