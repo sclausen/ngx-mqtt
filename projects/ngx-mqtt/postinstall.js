5cff5247127af52ac88d76d0af923b9c04742c7f
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2021-03-24 17:14:17 +0200

    fix(#169): change main for mqtt package

--- projects/ngx-mqtt/postinstall.js
+++ projects/ngx-mqtt/postinstall.js
@@ -1,47 +1,16 @@
-const path = require("path");
-const fs = require("fs");
-const semver_1 = require("semver");
-if (process.env["MQTT_DISABLE_HOOK"]) {
-  console.info("NGX-MQTT: postinstall hook disabled, MQTT_DISABLE_HOOK");
+const fs = require('fs');
+if (process.env.MQTT_DISABLE_HOOK) {
+  console.info('NGX-MQTT: postinstall hook disabled, MQTT_DISABLE_HOOK');
   return 0;
 }
 
-let f =
-  "../../node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
-const cwd = process.cwd();
-const packageJsonPath = path.join(
-  cwd,
-  "node_modules/@angular/core/package.json"
-);
-if (fs.existsSync(packageJsonPath)) {
-  const content = fs.readFileSync(packageJsonPath, "utf-8");
-  if (content) {
-    const { version } = JSON.parse(content);
-    if (version) {
-      let semv = new semver_1.SemVer(version);
-      if (semv.major >= 11) {
-        f =
-          "../../node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js";
-      }
-    }
-  }
-}
+const mqttPackageJsonPath = require.resolve('mqtt/package.json');
 
-fs.readFile(f, "utf8", function (err, data) {
+const mqttPackageJson = require(mqttPackageJsonPath);
+mqttPackageJson.main = 'dist/mqtt.js';
+fs.writeFile(mqttPackageJsonPath, JSON.stringify(mqttPackageJson), 'utf8', function (err) {
   if (err) {
     console.error(err);
     throw err;
   }
-  // at some moment angular-cli tam disabled node polyfills and stubs in webpack
-  // from:
-  // node: false
-  // to:
-  // node: {global: true}
-  const result = data.replace(/node: false/g, "node: {global: true}");
-  fs.writeFile(f, result, "utf8", function (err) {
-    if (err) {
-      console.error(err);
-      throw err;
-    }
-  });
 });
