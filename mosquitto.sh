f6f115b4a84fbbacfb1cf572df987531d57e5b90
Sebastian Clausen <github@sebastian-clausen.de>
2021-06-13 14:42:01 +0200

    extend mosquitto config

--- mosquitto.sh
+++ mosquitto.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-docker run -it --name mosquitto --rm -p 9001:9001 -v $(pwd)/mosquitto:/mosquitto/ eclipse-mosquitto
+docker run -it --name mosquitto --rm -p 9001:9001 -p 1883:1883 -v $(pwd)/mosquitto:/mosquitto/ eclipse-mosquitto
