126595d673047ed36049d4e2b24efd26dfa4046c
Sebastian Clausen <github@sebastian-clausen.de>
2024-02-05 17:31:28 +0100

    feat: extended readme

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { MqttModule } from 'ngx-mqtt';
 
 import { AppComponent } from './app.component';
 import { PublishComponent } from './publish.component';
-import { FormsModule } from '@angular/forms';
 import { SubscribeComponent } from './subscribe.component';
 
 @NgModule({
